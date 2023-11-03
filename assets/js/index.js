e12875afd28096deed41f6598da0e574a5fcc7b9
Bruno Paes <bruno.campiol@gmail.com>
2023-11-02 23:53:44 -0300

    add county flag to weather info

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,20 @@
 const fetchData = async () => {
-    try {
-      const response = await fetch('https://brunocampiol.online/api/Weather/FromContextIpAddress');
-      
-      if (response.status === 200) {
-        const data = await response.json();
-        const { englishName, weatherText, temperature } = data;
-        const weatherData = `${englishName} <br /> ${weatherText} - ${temperature}`;
-        document.getElementById('weatherData').innerHTML = weatherData;
-      } else {
-        console.error(`Request failed with status: ${response.status}`);
-      }
-    } catch (error) {
-      console.error('An error occurred while fetching data:', error);
+  try {
+    const response = await fetch('https://brunocampiol.online/api/Weather/FromContextIpAddress');
+    
+    if (response.status === 200) {
+      const data = await response.json();
+      const { englishName, countryCode, weatherText, temperature  } = data;
+      const flagUrl = `https://flagcdn.com/w20/${countryCode.toLowerCase()}.png`;
+      const flagImage = `<img src="${flagUrl}" alt="${countryCode}" />`;
+      const weatherData = `${englishName} - ${flagImage} <br /> ${weatherText} - ${temperature}`;
+      document.getElementById('weatherData').innerHTML = weatherData;
+    } else {
+      console.error(`Request failed with status: ${response.status}`);
     }
+  } catch (error) {
+    console.error('An error occurred while fetching data:', error);
+  }
 };
-  
+
 fetchData();
